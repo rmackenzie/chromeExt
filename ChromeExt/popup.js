383789df2550c3c6271b5cc1395282bdff0ee61d
Ronald Mackenzie <ron.mackenzie+ghc@gmail.com>
2014-04-08 01:33:07 -0400

    initial commit

--- ChromeExt/popup.js
+++ ChromeExt/popup.js
@@ -1,3 +1,10 @@
 //popup.js
 //javascript for the Twitch Streamer plugin for chrome
 
+var apiurl = 'https://api.twitch.tv/kraken/games/top';
+var twitch = json_decode(file_get_contents(apiurl), true);
+document.write("?");
+for (var i = 0; i < twitch.length; i++)
+{
+	document.write("hi");
+}
